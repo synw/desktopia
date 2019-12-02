734b23a6cef3a4ad95c7460e966fc773a8abdb2a
synw <synw@yahoo.com>
2019-12-02 16:12:56 +0100

    Export widgets

--- lib/desktopia.dart
+++ lib/desktopia.dart
@@ -1,7 +1,7 @@
 library desktopia;
 
-export 'src/widgets/widgets.dart';
 export 'src/components/config_manager.dart';
 export 'src/components/keyboard_events/keyboard_events.dart';
 export 'src/components/zones/models.dart';
 export 'src/components/zones/store.dart';
+export 'src/widgets/widgets.dart';
